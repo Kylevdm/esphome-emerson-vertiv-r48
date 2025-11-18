fc7fedd3832d2d7e95e67c2da03d2361285d052b
kylevdm <kyle@vandermerwes.co.uk>
2025-11-18 23:18:34 +0000

    updated ready for ESPHome Builder 2025.11

--- components/emerson_r48/button/__init__.py
+++ components/emerson_r48/button/__init__.py
@@ -15,12 +15,11 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(CONF_EMERSON_R48_ID): cv.use_id(EmersonR48Component),
-            cv.Optional(CONF_SET_OFFLINE_VALUES): button.BUTTON_SCHEMA.extend(
+            cv.Optional(CONF_SET_OFFLINE_VALUES): button.button_schema(
+                EmersonR48Button,
+                entity_category=ENTITY_CATEGORY_CONFIG,
+            ).extend(
                 {
-                    cv.GenerateID(): cv.declare_id(EmersonR48Button),
-                    cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
-                    ): cv.entity_category,
                     cv.Required('name'): cv.string_strict
                 }
             ),
@@ -28,15 +27,11 @@ CONFIG_SCHEMA = cv.All(
     ).extend(cv.COMPONENT_SCHEMA)
 )
 
-
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_EMERSON_R48_ID])
-    if config[CONF_SET_OFFLINE_VALUES]:
+    if CONF_SET_OFFLINE_VALUES in config:
         conf = config[CONF_SET_OFFLINE_VALUES]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
-        await button.register_button(
-            var,
-            conf,
-        )
+        await button.register_button(var, conf)
         cg.add(var.set_parent(hub))
--- components/emerson_r48/number/__init__.py
+++ components/emerson_r48/number/__init__.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import number
 from esphome.const import (
     UNIT_VOLT,
+    UNIT_PERCENT,  # Add this
     CONF_ID,
     CONF_ICON,
     CONF_UNIT_OF_MEASUREMENT,
@@ -23,7 +24,6 @@ CONF_OUTPUT_VOLTAGE = "output_voltage"
 CONF_MAX_OUTPUT_CURRENT = "max_output_current"
 CONF_MAX_INPUT_CURRENT = "max_input_current"
 
-
 EmersonR48Number = emerson_r48_ns.class_(
     "EmersonR48Number", number.Number, cg.Component
 )
@@ -32,68 +32,58 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(CONF_EMERSON_R48_ID): cv.use_id(EmersonR48Component),
-            cv.Optional(CONF_OUTPUT_VOLTAGE): number.NUMBER_SCHEMA.extend(
+            cv.Optional(CONF_OUTPUT_VOLTAGE): number.number_schema(
+                EmersonR48Number,
+                icon=ICON_FLASH,
+                unit_of_measurement=UNIT_VOLT,
+                entity_category=ENTITY_CATEGORY_NONE,
+            ).extend(
                 {
-                    cv.GenerateID(): cv.declare_id(EmersonR48Number),
                     cv.Optional(CONF_MIN_VALUE, default=41): cv.float_,
                     cv.Optional(CONF_MAX_VALUE, default=58.5): cv.float_,
                     cv.Optional(CONF_STEP, default=0.1): cv.float_,
-                    cv.Optional(CONF_ICON, default=ICON_FLASH): cv.icon,
-                    cv.Optional(
-                        CONF_UNIT_OF_MEASUREMENT, default=UNIT_VOLT
-                    ): cv.string_strict,
                     cv.Optional(CONF_MODE, default="BOX"): cv.enum(
                         number.NUMBER_MODES, upper=True
                     ),
-                    cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_NONE
-                    ): cv.entity_category,
                 }
             ),
-            cv.Optional(CONF_MAX_OUTPUT_CURRENT): number.NUMBER_SCHEMA.extend(
+            cv.Optional(CONF_MAX_OUTPUT_CURRENT): number.number_schema(
+                EmersonR48Number,
+                icon=ICON_FLASH,
+                unit_of_measurement=UNIT_PERCENT,
+                entity_category=ENTITY_CATEGORY_NONE,
+            ).extend(
                 {
-                    cv.GenerateID(): cv.declare_id(EmersonR48Number),
                     cv.Optional(CONF_MIN_VALUE, default=10): cv.float_,
                     cv.Optional(CONF_MAX_VALUE, default=121): cv.float_,
-                    cv.Optional(CONF_STEP, default=0.1): cv.float_,
-                    cv.Optional(CONF_ICON, default=ICON_FLASH): cv.icon,
-                    cv.Optional(
-                        CONF_UNIT_OF_MEASUREMENT, default=UNIT_AMPERE
-                    ): cv.string_strict,
+                    cv.Optional(CONF_STEP, default=1): cv.float_,
                     cv.Optional(CONF_MODE, default="BOX"): cv.enum(
                         number.NUMBER_MODES, upper=True
                     ),
-                    cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_NONE
-                    ): cv.entity_category,
                 }
             ),
-            cv.Optional(CONF_MAX_INPUT_CURRENT): number.NUMBER_SCHEMA.extend(
+            cv.Optional(CONF_MAX_INPUT_CURRENT): number.number_schema(
+                EmersonR48Number,
+                icon=ICON_CURRENT_AC,
+                unit_of_measurement=UNIT_AMPERE,
+                entity_category=ENTITY_CATEGORY_NONE,
+            ).extend(
                 {
-                    cv.GenerateID(): cv.declare_id(EmersonR48Number),
                     cv.Optional(CONF_MIN_VALUE, default=0): cv.float_,
                     cv.Optional(CONF_MAX_VALUE, default=20): cv.float_,
                     cv.Optional(CONF_STEP, default=0.1): cv.float_,
-                    cv.Optional(CONF_ICON, default=ICON_CURRENT_AC): cv.icon,
-                    cv.Optional(
-                        CONF_UNIT_OF_MEASUREMENT, default=UNIT_AMPERE
-                    ): cv.string_strict,
                     cv.Optional(CONF_MODE, default="BOX"): cv.enum(
                         number.NUMBER_MODES, upper=True
                     ),
-                    cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_NONE
-                    ): cv.entity_category,
                 }
             ),
         }
     ).extend(cv.COMPONENT_SCHEMA)
 )
 
-
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_EMERSON_R48_ID])
-    if config[CONF_OUTPUT_VOLTAGE]:
+    if CONF_OUTPUT_VOLTAGE in config:
         conf = config[CONF_OUTPUT_VOLTAGE]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
@@ -106,7 +96,7 @@ async def to_code(config):
         )
         cg.add(getattr(hub, "set_output_voltage_number")(var))
         cg.add(var.set_parent(hub, 0x0))
-    if config[CONF_MAX_OUTPUT_CURRENT]:
+    if CONF_MAX_OUTPUT_CURRENT in config:
         conf = config[CONF_MAX_OUTPUT_CURRENT]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
@@ -119,7 +109,7 @@ async def to_code(config):
         )
         cg.add(getattr(hub, "set_max_output_current_number")(var))
         cg.add(var.set_parent(hub, 0x3))
-    if config[CONF_MAX_INPUT_CURRENT]:
+    if CONF_MAX_INPUT_CURRENT in config:
         conf = config[CONF_MAX_INPUT_CURRENT]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
