23c795c09aaabba40ecb807c7a0a56d4e9efd362
Kylevdm <kyle@vandermerwes.co.uk>
2025-11-19 12:31:26 +0000

    Update emerson_r48_number.cpp

--- components/emerson_r48/number/emerson_r48_number.cpp
+++ components/emerson_r48/number/emerson_r48_number.cpp
@@ -12,21 +12,21 @@ void EmersonR48Number::control(float value) {
   switch (this->functionCode_) {
     case SET_VOLTAGE_FUNCTION:
       parent_->set_output_voltage(value);
-      this->publish_state(value);
+      // Remove: this->publish_state(value);
       break;
     case SET_CURRENT_FUNCTION:
       parent_->set_max_output_current(value);
-      this->publish_state(value);
+      // Remove: this->publish_state(value);
       break;
     case SET_INPUT_CURRENT_FUNCTION:
       parent_->set_max_input_current(value);
-      this->publish_state(value);
+      // Remove: this->publish_state(value);
       break;
-
     default:
       break;
   }
 }
+
 
 }  // namespace emerson_r48
 }  // namespace esphome
