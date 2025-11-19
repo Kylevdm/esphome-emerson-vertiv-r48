8fd897af9c615248dd37c6bb34f8a5c732376d28
Kylevdm <kyle@vandermerwes.co.uk>
2025-11-19 19:43:33 +0000

    Add WalkInButton and RestartOvervoltageButton classes

--- components/emerson_r48/button/emerson_r48_button.h
+++ components/emerson_r48/button/emerson_r48_button.h
@@ -16,5 +16,31 @@ class EmersonR48Button : public button::Button, public Component {
   void press_action() override;
 };
 
-}  // namespace emesron_r48
+class WalkInButton : public button::Button, public Component {
+ public:
+  void set_parent(EmersonR48Component *parent) { this->parent_ = parent; };
+  void set_enable(bool enable) { this->enable_ = enable; };
+  void set_time(float time) { this->time_ = time; };
+
+ protected:
+  EmersonR48Component *parent_;
+  bool enable_ = false;
+  float time_ = 0.0;
+
+  void press_action() override;
+};
+
+class RestartOvervoltageButton : public button::Button, public Component {
+ public:
+  void set_parent(EmersonR48Component *parent) { this->parent_ = parent; };
+  void set_enable(bool enable) { this->enable_ = enable; };
+
+ protected:
+  EmersonR48Component *parent_;
+  bool enable_ = false;
+
+  void press_action() override;
+};
+
+}  // namespace emerson_r48
 }  // namespace esphome
