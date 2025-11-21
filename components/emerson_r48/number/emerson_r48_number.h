7eaafc14d3d279f95f179c336a22b61f3a6a7dd6
Kylevdm <kyle@vandermerwes.co.uk>
2025-11-21 00:18:51 +0000

    Refactor EmersonR48Number methods and member variables
    Removed functionCode parameter from set_parent method and added set_type method to set the type of the component.

--- components/emerson_r48/number/emerson_r48_number.h
+++ components/emerson_r48/number/emerson_r48_number.h
@@ -8,14 +8,17 @@ namespace emerson_r48 {
 
 class EmersonR48Number : public number::Number, public Component {
  public:
-  void set_parent(EmersonR48Component *parent, int8_t functionCode) {
+  void set_parent(EmersonR48Component *parent) { 
     this->parent_ = parent;
-    this->functionCode_ = functionCode;
-  };
+  }
+  
+  void set_type(int8_t type) {
+    this->type_ = type;
+  }
 
  protected:
   EmersonR48Component *parent_;
-  int8_t functionCode_;
+  int8_t type_;  // 0 = voltage, 1 = current
 
   void control(float value) override;
 };
